d4d981cda22774f8db62e82b20cdf911a0826d2d
sergisergio <ptraon@pm.me>
2021-08-07 11:07:56 +0200

    add banner

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import os
 
 # Permet de connaître le répertoire courant
 print(os.getcwd())
+
+def banner():
+    banner = """\033[92m
+ |__ /__|_  )__/ | | _ ) __ _ __| |___  _ _ __  | _ \_  _| |___ 
+  |_ \___/ /___| | | _ \/ _` / _| / / || | '_ \ |   / || | / -_)
+ |___/  /___|  |_| |___/\__,_\__|_\_\\_,_| .__/ |_|_\\_,_|_\___|
+                                         |_|                    
+ \033[0m"""
+    return banner
