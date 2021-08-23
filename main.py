cc2ac22321f8d731407d596f08a22fbf8d76cefe
sergisergio <ptraon@pm.me>
2021-08-23 13:21:28 +0200

    test restore

--- main.py
+++ main.py
@@ -232,7 +232,7 @@ def restore_from_local():
     ssh.exec_command("rm /home/philippe/P6/backup/*.sql")
     ssh.exec_command("rm -R /home/philippe/P6/backup/" + file_to_restore)
     ssh.exec_command("rm /home/philippe/P6/backup/*.sql")
-    ssh.exec_command("chmod 755 -R /home/philippe/P6/backup/* && cd /home/philippe/P6/backup/20* && sudo cp * /var/www/html/ -R")
+    ssh.exec_command("chmod 775 -R /home/philippe/P6/backup/* && cd /home/philippe/P6/backup/20* && sudo cp * /var/www/html/ -R")
     #ssh.exec_command("rm -R /home/philippe/P6/backup/*")
 
     ssh.close()
