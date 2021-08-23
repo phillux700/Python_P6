65972eadac89f9f205922ed23c65461bf3ef07ef
sergisergio <ptraon@pm.me>
2021-08-23 14:19:03 +0200

    test restore

--- main.py
+++ main.py
@@ -228,12 +228,12 @@ def restore_from_local():
     ssh.exec_command("tar -xzvf /home/philippe/P6/backup/" + file_to_restore + " -C /home/philippe/P6/backup/")
     os.system("sleep 5")
     print('File extraction successful')
-    ssh.exec_command("mysql -u philippe -p password wordpress_db < /home/philippe/P6/backup/*.sql")
-    ssh.exec_command("rm /home/philippe/P6/backup/*.sql")
+    #ssh.exec_command("mysql -u philippe -p password wordpress_db < /home/philippe/P6/backup/*.sql")
+    #ssh.exec_command("rm /home/philippe/P6/backup/*.sql")
     #ssh.exec_command("rm -R /home/philippe/P6/backup/" + file_to_restore)
     #ssh.exec_command("rm /home/philippe/P6/backup/*.sql")
-    ssh.exec_command("sudo cp -rf /home/philippe/P6/backup/* /var/www/html/")
-    ssh.exec_command("rm -R /home/philippe/P6/backup/*")
+    #ssh.exec_command("sudo cp -rf /home/philippe/P6/backup/* /var/www/html/")
+    #ssh.exec_command("rm -R /home/philippe/P6/backup/*")
 
     ssh.close()
 
