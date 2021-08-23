171a4432cb522764c001cc5a33c19e92333017e1
sergisergio <ptraon@pm.me>
2021-08-23 11:38:30 +0200

    new extract test

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ def restore_from_local():
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname='192.168.1.4', username=username, password=password)
-    ssh.exec_command("tar -xzvf /home/philippe/P6/backup/" + file_to_restore )
+    ssh.exec_command("tar -xzvf /home/philippe/P6/backup/" + file_to_restore + " -C /home/philippe/P6/backup")
     print('File extraction successful')
     ssh.close()
 
