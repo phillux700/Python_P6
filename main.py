c266701d09e76bfd09746f35ca5c680cf1db6d16
sergisergio <ptraon@pm.me>
2021-08-23 22:01:41 +0200

    test restore

--- main.py
+++ main.py
@@ -245,7 +245,8 @@ def restore_from_remote():
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname='192.168.2.2', username=username, password=password)
-    backups = ssh.exec_command("ls /home/philippe/P6/backup")
+    #backups = ssh.exec_command("ls /home/philippe/P6/backup")
+    backups = ssh.exec_command(os.listdir("/home/philippe/P6/backup"))
     number = 0
     while number < len(backups):
         number = number + 1
