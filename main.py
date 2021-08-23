eb5bb587c3d6dd0adb4398a07df5b48246a2c499
sergisergio <ptraon@pm.me>
2021-08-23 11:58:29 +0200

    new extract test

--- main.py
+++ main.py
@@ -223,12 +223,12 @@ def restore_from_local():
     os.system("sleep 5")
     print('File has been sent ...')
 
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname='192.168.1.4', username=username, password=password)
-    ssh.exec_command("tar -xzvf /home/philippe/P6/backup/" + file_to_restore + " -C /home/philippe/P6/backup")
-    print('File extraction successful')
-    ssh.close()
+    # ssh = paramiko.SSHClient()
+    # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # ssh.connect(hostname='192.168.1.4', username=username, password=password)
+    # ssh.exec_command("tar -xzvf /home/philippe/P6/backup/" + file_to_restore + " -C /home/philippe/P6/backup")
+    # print('File extraction successful')
+    # ssh.close()
 
 
     #### TODO Le fichier est envoyé: il reste la restauration à faire
