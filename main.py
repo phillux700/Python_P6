5a28d12549f46a90e20e2dcba65311f2c9a523de
sergisergio <ptraon@pm.me>
2021-08-30 12:49:54 +0200

    test restore

--- main.py
+++ main.py
@@ -269,7 +269,7 @@ def restore_from_remote():
     backup_choice = input(show_input())
     file_to_restore = stdout[int(backup_choice) - 1]
     print("Vous avez choisi la sauvegarde " + file_to_restore)
-    ssh.exec_command("sftp philippe@192.168.1.4:/home/philippe/P6 <<< $'put " + "/home/philippe/P6/backup/" + file_to_restore + "'")
+    ssh.exec_command("sftp philippe:password@192.168.1.4:/home/philippe/P6 <<< $'put " + "/home/philippe/P6/backup/" + file_to_restore + "'")
     #### TODO Régler le pb de password, vérifier que le serveur wordpress reçoive le fichier
     ssh.exec_command("sleep 5")
     ssh.close()
