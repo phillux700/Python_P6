7c257a646dfe9b9ad4cf5555a5c1b65dc653775e
sergisergio <ptraon@pm.me>
2021-08-30 21:21:29 +0200

    new restore test

--- main.py
+++ main.py
@@ -270,7 +270,7 @@ def restore_from_remote():
     localFile = "/home/philippe/P6/backup/" + file_to_restore
     os.system("touch " + localFile)
     if os.path.isfile(localFile):
-        sftp.get("/home/philippe/P6/tmp/" + file_to_restore, localFile)
+        sftp.get(localFile, "/home/philippe/P6/tmp/" + file_to_restore, confirm=False)
     else:
         raise IOError('Could not find localFile %s !!' % localFile)
     sftp.close()
